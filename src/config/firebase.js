083ae93ce9b511ed924e158ad65ab53664ae3b68
lekoalak <lekoalakhotso@gmail.com>
2026-06-09 07:09:41 +0200

    fix: firebase auth AsyncStorage for mobile

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 ﻿import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBT73H0gzjh0T6dSofeT5VnBqFfzACkrio",
@@ -13,5 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 export default app;
